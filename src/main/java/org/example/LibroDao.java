cc65d74943908bd66b8d88500964f4ddd9090041
Mateo <mateomudano29@gmail.com>
2026-01-23 09:16:37 +0100

    añadido searchByTitulo en LibroDao

---
+++ src/main/java/org/example/LibroDao.java
@@ -0,0 +1,135 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class LibroDao {
+    private Libro mapRow(ResultSet rs) throws SQLException {
+        int id = rs.getInt("id");
+        String titulo = rs.getString("titulo");
+        String isbn = rs.getString("isbn");
+        Integer anio = (Integer) rs.getObject("anio"); // admite NULL
+        boolean disponible = rs.getBoolean("disponible");
+        return new Libro(id, titulo, isbn, anio, disponible);
+    }
+
+    public List<Libro> findAll() {
+        String sql = "SELECT id, titulo, isbn, anio, disponible FROM libro ORDER BY id ";
+        try (Connection con = Db.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            List<Libro> libros = new ArrayList<>();
+            while (rs.next()) {
+                libros.add(mapRow(rs));
+            }
+            return libros;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error leyendo libros", e);
+        }
+    }
+
+    public Optional<Libro> findById(int id) {
+        String sql = "SELECT id, titulo, isbn, anio, disponible FROM libro WHERE id = ? ";
+        try (Connection con = Db.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return Optional.of(mapRow(rs));
+                }
+                return Optional.empty();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error buscando libro por id=" +
+                    id, e);
+        }
+    }
+
+    public int insert(Libro libro) {
+        String sql = "INSERT INTO libro (titulo, isbn, anio, disponible) VALUES( ?,?,?,?)";
+        try (Connection con = Db.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql,
+                     Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, libro.getTitulo());
+            ps.setString(2, libro.getIsbn());
+            ps.setObject(3, libro.getAnio()); // admite NULL
+            ps.setBoolean(4, libro.isDisponible());
+            int rows = ps.executeUpdate();
+            if (rows != 1) {
+                throw new SQLException("INSERT inesperado. Filas afectadas:" + rows);
+            }
+            try (ResultSet keys = ps.getGeneratedKeys()) {
+                if (keys.next()) {
+                    int id = keys.getInt(1);
+                    libro.setId(id);
+                    return id;
+                }
+                throw new SQLException("No se devolvió id generado (getGeneratedKeys vacío)");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error insertando libro", e);
+        }
+    }
+
+    public boolean update(Libro libro) {
+        if (libro.getId() == null) {
+            throw new IllegalArgumentException("No se puede UPDATE sin id. ¿Hiciste insert primero ? ");
+        }
+        String sql = "UPDATE libro SET titulo=?, isbn=?, anio=?, disponible = ? WHERE id = ? ";
+        try (Connection con = Db.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, libro.getTitulo());
+            ps.setString(2, libro.getIsbn());
+            ps.setObject(3, libro.getAnio());
+            ps.setBoolean(4, libro.isDisponible());
+            ps.setInt(5, libro.getId());
+            return ps.executeUpdate() == 1;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error actualizando libro id=" + libro.getId(), e);
+        }
+    }
+
+    public boolean deleteById(int id) {
+        String sql = "DELETE FROM libro WHERE id=?";
+        try (Connection con = Db.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            return ps.executeUpdate() == 1;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error borrando libro id=" + id, e);
+        }
+    }
+
+    public List<Libro> searchByTitulo(String texto) {
+        // Consulta sql  usando lower, y ? como placeholder
+        String sql = "SELECT id, titulo, isbn, anio, disponible " +
+                "FROM libro " +
+                "WHERE lower(titulo) LIKE lower(?) " +
+                "ORDER BY id";
+
+        // Obtiene la conexion y prepara el sql
+        try (Connection con = Db.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, "%" + texto + "%");
+
+            // ejecuta el sql e imprime los resultados utilizando el "mapRow()"
+            try (ResultSet rs = ps.executeQuery()) {
+                List<Libro> res = new ArrayList<>();
+                while (rs.next()) {
+                    res.add(mapRow(rs));
+                }
+                return res;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error buscando por título: " +
+                    texto, e);
+        }
+    }
+
+}
